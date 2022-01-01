3e1e4efb01470b06a880f52966bca4913479a618
Justin Guese <guese.justin@gmail.com>
2022-01-01 14:30:33 +0100

    proven install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # unpack files
 # tar -zxvf asana.tar.gz
-cat asana.tar.gz.* | tar xzvf -
+cat asana.tar* | tar xzvf -
 # permissions (might not work if not sudo)
 cp -r Asana-linux-x64 $HOME/
 cd $HOME/Asana-linux-x64/
@@ -18,5 +18,5 @@ Type=Application
 Icon=$HOME/Asana-linux-x64/resources/app/icon.png
 Categories=Office;ProjectManagement;" > "asana.desktop"
 # copy desktop file to applications
-cp asana.desktop $HOME/.local/share/applications
+cp ./asana.desktop $HOME/.local/share/applications
 echo "Sucessfully installed Asana. Type Asana into your menu to launch"
